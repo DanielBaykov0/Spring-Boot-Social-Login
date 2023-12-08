3412c2f99ac5be5e1fec706ba277a6b4ed1ec0ed
Daniel Baykov <danibaikov@gmail.com>
2023-12-08 21:36:02 +0200

    Initial commit with added dependencies and application.properties configuration

---
+++ src/main/java/baykov/daniel/sociallogin/SocialloginApplication.java
@@ -0,0 +1,12 @@
+package baykov.daniel.sociallogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialloginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialloginApplication.class, args);
+    }
+}
