b3d9098b7d502d5cf5875c959ae94b731f8b4717
Daniel Baykov <danibaikov@gmail.com>
2023-12-08 21:43:50 +0200

    adding user repository

---
+++ src/main/java/baykov/daniel/sociallogin/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package baykov.daniel.sociallogin.repository;
+
+import baykov.daniel.sociallogin.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    Boolean existsByEmail(String email);
+}
